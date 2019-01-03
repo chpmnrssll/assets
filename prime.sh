6c3dad422c3845222244766525b42d054862a956
Russell Chapman <chpmnrssll@gmail.com>
2019-01-03 01:26:58 -0700

    Update assets, set ffmpeg frame-rate to 10fps in prime.sh

--- prime.sh
+++ prime.sh
@@ -10,4 +10,4 @@ do
 primitive -i $FILENAME.* -m 8 -n $NUM -o $FILENAME$frame.png -s $SIZE -v
 done
 
-ffmpeg -r 60 -f image2 -i $FILENAME%d.png -vcodec libx264 -crf 15 -pix_fmt yuv420p $FILENAME.mp4
+ffmpeg -r 10 -f image2 -i $FILENAME%d.png -vcodec libx264 -crf 15 -pix_fmt yuv420p $FILENAME.mp4
